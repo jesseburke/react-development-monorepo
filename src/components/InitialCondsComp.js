8e801d6bf790330ee74a9eaf945571bba8b4a0ce
jesseburke <jtb445@gmail.com>
2020-03-26 18:11:30 +1100

    fixed bug where changes in props were not displayed

--- src/components/InitialCondsComp.js
+++ src/components/InitialCondsComp.js
@@ -4,15 +4,10 @@ import Input from '../components/Input.js';
 
 function InitialCondsComp({ initialConds=[[1,0],[1,1]], changeCB = () => null}) {
 
-    const [firstPt, setFirstPt] = useState(initialConds[0]);
-    const [secPt, setSecPt] = useState(initialConds[1]);
+    let firstPt = initialConds[0];
+    let secPt = initialConds[1];
 
-    useEffect( () => {
-
-        changeCB([ firstPt, secPt ]);
-        //console.log('effect inside initialCondsComp called');
-        
-    }, [firstPt, secPt] );
+    
 
     return (
         <div css={{
@@ -36,20 +31,32 @@ function InitialCondsComp({ initialConds=[[1,0],[1,1]], changeCB = () => null}) 
             <div css={{padding: '.25em 0'}}>
               <span>y(</span>
               <Input initValue={firstPt[0]}
-                     onC={useCallback( val => setFirstPt( pt => [Number(val), pt[1]] ) )}
+                     onC={useCallback( val => {
+                         firstPt = [Number(val), firstPt[1]];
+                         changeCB([ firstPt, secPt ]);
+                     })}
                      size={4}/> <span>) = </span>
               <Input initValue={firstPt[1]}
-                     onC={useCallback( val => setFirstPt( pt => [pt[0], Number(val)] ) )}
+                     onC={useCallback( val => {
+                         firstPt = [firstPt[0], Number(val)];
+                         changeCB([ firstPt, secPt ]);
+                     })}                    
                      size={4}/>
             </div>
              <div css={{padding: '.25em 0'}}>
               <span>y'(</span>
                <Input initValue={secPt[0]}
-                      onC={useCallback( val => setSecPt( pt => [Number(val), pt[1]] ) )}
+                      onC={useCallback( val => {
+                          secPt = [Number(val), secPt[1]];
+                          changeCB([ firstPt, secPt ]);
+                      })}                    
                       size={4}/>
                <span>) = </span>
                <Input initValue={secPt[1]}
-                      onC={useCallback( val => setSecPt( pt => [pt[0], Number(val)] ) )}
+                      onC={useCallback( val => {
+                          secPt = [secPt[0], Number(val)];
+                          changeCB([ firstPt, secPt ]);
+                      })}                    
                       size={4}/>
             </div>
           </div>
