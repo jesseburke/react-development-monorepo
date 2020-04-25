7d24ea8d3f6ac33ea195efd139ed90e2a47ff597
jesseburke <jtb445@gmail.com>
2020-04-25 19:04:03 +1000

    added FuncParserXT, which uses xt coords, instead of xy

--- src/utils/funcParser.js
+++ src/utils/funcParser.js
@@ -9,3 +9,13 @@ export default function funcParser(funcStr) {
     return ((x0,y0) => parser.evaluate(funcStr, {x:x0, y:y0}));
 
 }
+
+export function funcParserXT(funcStr) {
+
+    const parser = new Parser();
+
+    parser.consts.e = Math.E;
+    
+    return ((x0,t0) => parser.evaluate(funcStr, {x:x0, t:t0}));
+
+}
