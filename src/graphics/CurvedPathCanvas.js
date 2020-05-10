98208d9e5685351675a817ad1e858369c911c276
jesseburke <jtb445@gmail.com>
2020-05-10 17:04:42 +1000

    changes...

--- src/graphics/CurvedPathCanvas.js
+++ src/graphics/CurvedPathCanvas.js
@@ -1,8 +1,17 @@
 
 // compArray is an array of arrays; each array is a chain of points to be drawn
-export default function CurvedPathCanvas({ compArray, bounds, ctx }) {
+export default function CurvedPathCanvas({ compArray, bounds, color, lineWidth }) {
+
+    const ctx = document.createElement('canvas').getContext('2d');
+    ctx.canvas.width = 1024;
+    ctx.canvas.height = 1024;
+    ctx.strokeStyle = color;
+    ctx.lineWidth = lineWidth;
 
     let {xMin, xMax, zMin, zMax} = bounds;
+
+    const xRange = xMax - xMin;
+    const zRange = zMax - zMin;
 
     if( !zMax ) zMax = 10;
     if( !zMin ) zMin = -10;
@@ -14,24 +23,8 @@ export default function CurvedPathCanvas({ compArray, bounds, ctx }) {
     const cx = .1;
     const cz = .5;
 
-    function sceneToCanvas ([x,z]) {
-	return [ cx*w + ((1-cx)*w)*(x/xMax),
-		 (1-(z/zMax))*h*cz ];
-    }
 
-    // draw axes
-    ctx.beginPath();
-    ctx.moveTo( ...sceneToCanvas([-1,0]) );
-    ctx.lineTo( ...sceneToCanvas([xMax,0]) );
-    //ctx.closePath();
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo( ...sceneToCanvas([0,zMin]) );
-    ctx.lineTo( ...sceneToCanvas([0,zMax]) );
-    //ctx.closePath();
-    ctx.stroke();
-
+    const stc = ([x,z]) => ([ ((x-xMin)/xRange)*w, (1-((z-zMin)/zRange))*h ]);
     
     let curve, curArray, nextPt, l, tempD;
     
@@ -41,17 +34,19 @@ export default function CurvedPathCanvas({ compArray, bounds, ctx }) {
      	l = curArray.length;
 
      	ctx.beginPath();
-     	ctx.moveTo( ...sceneToCanvas(curArray[0]) );
+     	ctx.moveTo( ...stc(curArray[0]) );
 
      	for( let i = 1; i < l; i++ ) {
 
-     	    ctx.lineTo(  ...sceneToCanvas(curArray[i]) );
+     	    ctx.lineTo(  ...stc(curArray[i]) );
 	
 	}
 
 	//ctx.closePath();
 	ctx.stroke();
     }
+
+    return ctx;
 
     //unchangeCanvasCoords( ctx, bounds );
     
