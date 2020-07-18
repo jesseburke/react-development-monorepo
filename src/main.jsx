d7af33358b019d378b591b3b425892045e2d446b
jesseburke <jtb445@gmail.com>
2020-07-18 21:48:22 +1000

    changed App

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { Helmet } from 'react-helmet';
 
 import './index.css';
-import App from './pages/df/App.jsx';
+import App from './pages/df/App_sep.jsx';
 
 function main() {
     const element = document.createElement('div');
