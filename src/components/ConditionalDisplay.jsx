54556901163dd2882d6330651727da728dd471b5
jesseburke <jtb445@gmail.com>
2021-02-09 20:47:14 +1100

    deleted unused files

--- src/components/ConditionalDisplay.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ConditionalDisplay({ test, children }) {
-    if (test) {
-        return <div>{children}</div>;
-    } else {
-        return null;
-    }
-}
