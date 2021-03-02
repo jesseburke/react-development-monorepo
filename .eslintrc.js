376f64ebb04f3417796bb11da0ee151d161e6fd0
jesseburke <jtb445@gmail.com>
2021-03-02 15:05:54 +1100

    added dot files

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    env: {
+        es2020: true
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 11,
+        sourceType: 'module'
+    },
+    plugins: ['react'],
+    rules: {}
+};
