4eb68dd10a547e0447fa31d24e3de149ebf830f5
jesseburke <jtb445@gmail.com>
2021-03-30 20:08:25 +1100

    removed build options from vite config

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ const { resolve } = require('path');
 
 export default defineConfig({
     base: '/u/jburke/directionFieldGrapher/',
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                df: resolve(__dirname, 'src/pages/df/App_df'),
-                sep: resolve(__dirname, 'src/pages/df/App_sep')
-            }
-        }
-    },
     plugins: [reactRefresh()],
     optimizeDeps: {
         include: [
