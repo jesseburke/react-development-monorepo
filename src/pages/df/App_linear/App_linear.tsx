1d967581570960cbe32c2442146b564d3f917ee9
jesseburke <jtb445@gmail.com>
2021-06-02 14:51:43 +1000

    updated App_linear to use new data system

---
+++ src/pages/df/App_linear/App_linear.tsx
@@ -0,0 +1,179 @@
+import React, { useState, useRef, useEffect, useCallback } from 'react';
+
+import * as THREE from 'three';
+
+import { atom, useAtom, Provider as JProvider } from 'jotai';
+
+import { useDialogState, Dialog, DialogDisclosure } from 'reakit/Dialog';
+import { Provider } from 'reakit/Provider';
+import { useTabState, Tab, TabList, TabPanel } from 'reakit/Tab';
+import * as system from 'reakit-system-bootstrap';
+
+import { ThreeSceneComp } from '../../../components/ThreeScene';
+import ClickablePlaneComp from '../../../components/RecoilClickablePlaneComp.jsx';
+
+import Grid from '../../../ThreeSceneComps/Grid';
+import Axes2D from '../../../ThreeSceneComps/Axes2DRecoil.jsx';
+import ArrowGrid from '../../../ThreeSceneComps/ArrowGridRecoil.jsx';
+import IntegralCurve from '../../../ThreeSceneComps/IntegralCurve';
+import CameraControls from '../../../ThreeSceneComps/CameraControls.jsx';
+
+import {
+    arrowGridData,
+    boundsData,
+    initialPointData,
+    funcAtom,
+    labelData,
+    solutionCurveData,
+    axesData,
+    orthoCameraData,
+    DataComp,
+    LinearEquationInput
+} from './App_linear_atoms.jsx';
+
+//------------------------------------------------------------------------
+//
+// initial data
+//
+
+const initControlsData = {
+    mouseButtons: { LEFT: THREE.MOUSE.PAN },
+    touches: { ONE: THREE.MOUSE.PAN, TWO: THREE.TOUCH.DOLLY_PAN },
+    enableRotate: true,
+    enablePan: true,
+    enabled: true,
+    keyPanSpeed: 50,
+    screenSpaceSpanning: true
+};
+
+const aspectRatio = window.innerWidth / window.innerHeight;
+
+const fixedCameraData = {
+    up: [0, 1, 0],
+    near: 0.1,
+    far: 100,
+    aspectRatio,
+    orthographic: true
+};
+
+const saveBtnClassStr =
+    'absolute left-8 bottom-40 p-2 border med:border-2 rounded-md border-solid border-persian_blue-900 cursor-pointer text-xl';
+
+const resetBtnClassStr =
+    'absolute left-8 bottom-24 p-2 border med:border-2 rounded-md border-solid border-persian_blue-900 cursor-pointer text-xl';
+
+const photoBtnClassStr =
+    'absolute left-8 bottom-8 p-2 border med:border-2 rounded-md border-solid border-persian_blue-900 cursor-pointer text-xl';
+
+//------------------------------------------------------------------------
+
+export default function App() {
+    return (
+        <JProvider>
+            <div className='full-screen-base'>
+                <Provider unstable_system={system}>
+                    <header
+                        className='control-bar bg-persian_blue-900 font-sans
+			p-4 md:p-8 text-white'
+                    >
+                        <LinearEquationInput />
+                        <initialPointData.component />
+                        <OptionsModal />
+                    </header>
+
+                    <main className='flex-grow relative p-0'>
+                        <ThreeSceneComp
+                            fixedCameraData={fixedCameraData}
+                            controlsData={initControlsData}
+                            showPhotoButton={true}
+                            photoBtnClassStr={photoBtnClassStr}
+                        >
+                            <Grid boundsAtom={boundsData.atom} gridShow={true} />
+                            <Axes2D
+                                tickLabelDistance={1}
+                                boundsAtom={boundsData.atom}
+                                axesDataAtom={axesData.atom}
+                                labelAtom={labelData.atom}
+                            />
+                            <ArrowGrid
+                                diffEqAtom={funcAtom}
+                                boundsAtom={boundsData.atom}
+                                arrowGridDataAtom={arrowGridData.atom}
+                            />
+                            <IntegralCurve
+                                initialPointAtom={initialPointData.atom}
+                                boundsAtom={boundsData.atom}
+                                diffEqAtom={funcAtom}
+                                curveDataAtom={solutionCurveData.atom}
+                            />
+                            <ClickablePlaneComp clickPositionAtom={initialPointData.atom} />
+                            <CameraControls cameraDataAtom={orthoCameraData.atom} />
+                        </ThreeSceneComp>
+                        <DataComp
+                            resetBtnClassStr={resetBtnClassStr}
+                            saveBtnClassStr={saveBtnClassStr}
+                        />
+                    </main>
+                </Provider>
+            </div>
+        </JProvider>
+    );
+}
+
+function OptionsModal() {
+    const dialog = useDialogState();
+    const tab = useTabState();
+
+    useEffect(() => {
+        window.dispatchEvent(new Event('resize'));
+    });
+
+    const cssRef = useRef({
+        transform: 'none',
+        top: '15%',
+        left: 'auto',
+        backgroundColor: 'white',
+        right: 20,
+        width: 600,
+        height: 300
+    });
+
+    const cssRef1 = useRef({
+        backgroundColor: 'white',
+        color: '#0A2C3C'
+    });
+
+    return (
+        <div zindex={-10}>
+            <DialogDisclosure style={cssRef1.current} {...dialog}>
+                <span className='w-32'>{!dialog.visible ? 'Show options' : 'Hide options'}</span>
+            </DialogDisclosure>
+            <Dialog {...dialog} style={cssRef.current} aria-label='Welcome'>
+                <>
+                    <TabList {...tab} aria-label='Option tabs'>
+                        <Tab {...tab}>Arrow grid</Tab>
+                        <Tab {...tab}>Axes</Tab>
+                        <Tab {...tab}>Bounds</Tab>
+                        <Tab {...tab}>Solution curve</Tab>
+                        <Tab {...tab}>Variables</Tab>
+                    </TabList>
+                    <TabPanel {...tab}>
+                        <arrowGridData.component />
+                    </TabPanel>
+                    <TabPanel {...tab}>
+                        <axesData.component />
+                    </TabPanel>
+                    <TabPanel {...tab}>
+                        <boundsData.component />
+                    </TabPanel>
+                    <TabPanel {...tab}>
+                        <solutionCurveData.component />
+                    </TabPanel>
+                    <TabPanel {...tab}>
+                        <labelData.component />
+                    </TabPanel>
+                </>
+            </Dialog>
+        </div>
+    );
+}
---
+++ src/pages/df/App_linear/App_linear_atoms.jsx
@@ -0,0 +1,152 @@
+import React, { useState, useRef, useEffect, useCallback } from 'react';
+import { atom, useAtom } from 'jotai';
+
+import classnames from 'classnames';
+import styles from '../base_styles.module.css';
+
+import MainDataComp from '../../../data/MainDataComp.jsx';
+import LabelDataComp from '../../../data/LabelDataComp.jsx';
+import PointDataComp from '../../../data/PointDataComp.jsx';
+import FunctionDataComp from '../../../data/FunctionDataComp.jsx';
+import ArrowGridDataComp from '../../../data/ArrowGridDataComp.jsx';
+import AxesDataComp from '../../../data/AxesDataComp.jsx';
+import BoundsDataComp from '../../../data/BoundsDataComp';
+import CurveDataComp from '../../../data/CurveDataComp';
+import OrthoCameraDataComp from '../../../data/OrthoCameraDataComp';
+
+import TexDisplayComp from '../../../components/TexDisplayComp.jsx';
+
+import funcParser from '../../../utils/funcParser.jsx';
+import { round } from '../../../utils/BaseUtils.ts';
+
+//------------------------------------------------------------------------
+//
+// initial constants
+
+const colors = {
+    arrows: '#B01A46', //'#C2374F'
+    solutionCurve: '#4e6d87', //'#C2374F'
+    tick: '#e19662'
+};
+
+const initArrowData = { density: 1, thickness: 1, length: 0.75, color: colors.arrows };
+
+const initBounds = { xMin: -20, xMax: 20, yMin: -20, yMax: 20 };
+
+const initInitialPoint = { x: 0, y: 6 };
+
+const initSolutionCurveData = {
+    color: colors.solutionCurve,
+    approxH: 0.1,
+    visible: true,
+    width: 0.1
+};
+
+const initPXFuncStr = '4*x/(x^2+1)';
+const initQXFuncStr = '-12*x/(x^2+1)';
+
+const initAxesData = {
+    radius: 0.01,
+    show: true,
+    tickLabelDistance: 5
+};
+
+const initCameraData = {
+    center: [0, 0, 0],
+    zoom: 0.2,
+    position: [0, 0, 50]
+};
+
+export const labelStyle = {
+    color: 'black',
+    padding: '.1em',
+    margin: '.5em',
+    fontSize: '1.5em'
+};
+
+const tickLabelStyle = Object.assign(Object.assign({}, labelStyle), {
+    fontSize: '1em',
+    color: colors.tick
+});
+
+//------------------------------------------------------------------------
+//
+// primitive atoms
+
+export const labelData = LabelDataComp({ twoD: true });
+export const initialPointData = PointDataComp(initInitialPoint, 'Initial Point: ');
+export const arrowGridData = ArrowGridDataComp(initArrowData);
+export const axesData = AxesDataComp({
+    ...initAxesData,
+    tickLabelStyle
+});
+export const solutionCurveData = CurveDataComp(initSolutionCurveData);
+
+export const boundsData = BoundsDataComp({
+    initBounds,
+    labelAtom: labelData.atom
+});
+
+export const orthoCameraData = OrthoCameraDataComp(initCameraData);
+
+const pxFunctionData = FunctionDataComp({
+    initVal: initPXFuncStr,
+    functionLabelAtom: atom((get) => 'p(' + get(labelData.atom).x + ') = '),
+    labelAtom: labelData.atom,
+    inputSize: 10
+});
+
+const qxFunctionData = FunctionDataComp({
+    initVal: initQXFuncStr,
+    functionLabelAtom: atom((get) => 'q(' + get(labelData.atom).x + ') = '),
+    labelAtom: labelData.atom,
+    inputSize: 10
+});
+
+const atomStoreAtom = atom({
+    ls: labelData,
+    ip: initialPointData,
+    ag: arrowGridData,
+    ax: axesData,
+    sc: solutionCurveData,
+    bd: boundsData,
+    cd: orthoCameraData,
+    px: pxFunctionData,
+    qx: qxFunctionData
+});
+
+export const DataComp = MainDataComp(atomStoreAtom);
+
+//------------------------------------------------------------------------
+//
+// derived atoms
+
+export const funcAtom = atom((get) => ({
+    func: (x, y) =>
+        get(pxFunctionData.funcAtom).func(x, 0) * -y + get(qxFunctionData.funcAtom).func(x, 0)
+}));
+
+//------------------------------------------------------------------------
+//
+// input components
+
+export const LinearEquationInput = React.memo(function LinearEquationI({}) {
+    const { x: xLabel, y: yLabel } = useAtom(labelData.atom)[0];
+
+    const LatexLinearEquation = `\\frac{d${yLabel}}{d${xLabel}} + p(${xLabel})${yLabel} = q(${xLabel})`;
+    return (
+        <div className='flex flex-col justify-center items-center h-full'>
+            <div className='px-2 py-3'>
+                <TexDisplayComp str={LatexLinearEquation} />
+            </div>
+            <div className='flex flex-col md:flex-row'>
+                <span className='px-2 py-1'>
+                    <pxFunctionData.component />
+                </span>
+                <span className='px-2 py-1'>
+                    <qxFunctionData.component />
+                </span>
+            </div>
+        </div>
+    );
+});
