b93c9683198ae8e88c3fa407e15ba53f75565cbc
jesseburke <jtb445@gmail.com>
2022-01-16 16:46:17 +1100

    fixed bug that start time was unscaled

--- packages/jotai-data-setup/data-components/AnimationData.tsx
+++ packages/jotai-data-setup/data-components/AnimationData.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useState, useRef, useEffect, useCallback } from 'react';
+import React, { memo, useState, useRef, useEffect, useLayoutEffect, useCallback } from 'react';
 import { atom, useAtom } from 'jotai';
 import queryString from 'query-string-esm';
 import { useImmerAtom } from 'jotai/immer';
@@ -11,127 +11,131 @@ import { AnimationDataType } from '../my-types';
 
 import '../styles.css';
 
-const defaultInitValue: AnimationDataType = {
-    t: 0,
-    paused: true,
-    animationTime: 12
-    //min: 0,
-    //max: 20
-};
-
 // problem here: want to connect, e.g., min, to another piece of data
 
-export default function AnimationData({
-    animationTime = defaultInitValue.animationTime,
-    minMaxAtom
-}) {
-    const initValue = { ...defaultInitValue, animationTime };
+export default function AnimationData({ animationTime = 12, initT = -10, minMaxAtom }) {
+  const initValue = { animationTime, t: initT, paused: true };
 
-    const animationAtom = atom(initValue);
+  const animationAtom = atom(initValue);
 
-    const readWriteAtom = atom(null, (get, set, action) => {
-        switch (action.type) {
-            case 'reset':
-                set(animationAtom, initValue);
-                break;
+  const readWriteAtom = atom(null, (get, set, action) => {
+    switch (action.type) {
+      case 'reset':
+        set(animationAtom, initValue);
+        break;
 
-            case 'readAndEncode':
-                const { t, paused, animationTime } = diffObjects(get(animationAtom), initValue);
+      case 'readAndEncode':
+        const { t, paused, animationTime } = diffObjects(get(animationAtom), initValue);
 
-                let ro: AnimationDataType = {};
+        let ro: AnimationDataType = {};
 
-                if (t) ro.t = t;
-                if (paused) ro.p = paused ? 0 : 1;
-                if (animationTime) ro.a = animationTime;
+        if (t) ro.t = t;
+        if (paused) ro.p = paused ? 0 : 1;
+        if (animationTime) ro.a = animationTime;
 
-                if (isEmpty(ro)) return;
+        if (isEmpty(ro)) return;
 
-                action.callback(ro);
-                break;
+        action.callback(ro);
+        break;
 
-            case 'decodeAndWrite':
-                const rawObj: AnimationData = action.value;
+      case 'decodeAndWrite':
+        const rawObj: AnimationData = action.value;
 
-                const newKeys = Object.keys(rawObj);
+        const newKeys = Object.keys(rawObj);
 
-                const nro = {};
+        const nro = {};
 
-                if (newKeys.includes('t')) nro.t = Number(rawObj.t);
-                if (newKeys.includes('p')) nro.paused = rawObj.p === 0 ? false : true;
-                if (newKeys.includes('a')) nro.animationTime = Number(rawObj.a);
+        if (newKeys.includes('t')) nro.t = Number(rawObj.t);
+        if (newKeys.includes('p')) nro.paused = rawObj.p === 0 ? false : true;
+        if (newKeys.includes('a')) nro.animationTime = Number(rawObj.a);
 
-                set(animationAtom, { ...initValue, ...nro });
-                break;
+        set(animationAtom, { ...initValue, ...nro });
+        break;
+    }
+  });
+
+  const component = () => {
+    //----------------------------------------
+    //
+    // animationData related
+    //
+    const [animationData, setAnimationData] = useAtom(animationAtom);
+    const { t, paused, animationTime } = animationData;
+
+    const sliderCB = useCallback(
+      (newT) =>
+        setAnimationData((oldData) => ({
+          ...oldData,
+          paused: true,
+          t: round(Number(newT))
+        })),
+      []
+    );
+
+    const repeatCB = useCallback(
+      (newT) =>
+        setAnimationData((oldData) => ({
+          ...oldData,
+          t: min
+        })),
+      []
+    );
+
+    const updateCB = useCallback(
+      (newTime) =>
+        setAnimationData((oldData) => ({
+          ...oldData,
+          t: round(newTime * (max - min) + min)
+        })),
+      []
+    );
+
+    const pauseCB = useCallback(
+      () =>
+        setAnimationData((oldData) => ({
+          ...oldData,
+          paused: !oldData.paused
+        })),
+      []
+    );
+
+    //----------------------------------------
+
+    const { min, max } = useAtom(minMaxAtom)[0];
+
+    const [timeline, setTimeline] = useState(null);
+
+    useEffect(() => {
+      if (paused) {
+        if (timeline) {
+          timeline.pause();
         }
-    });
+        setTimeline(null);
+        return;
+      }
 
-    const component = () => {
-        //----------------------------------------
-        //
-        // animationData related
-        //
-        const [animationData, setAnimationData] = useImmerAtom(animationAtom);
-        const { t, paused, animationTime } = animationData;
+      const startTime = t < min ? min : t > max ? max : t;
+      const scaledStartTime = (t - min) / (max - min);
 
-        const sliderCB = useCallback((newT) => {
-            setAnimationData((draft) => {
-                draft.paused = true;
-                draft.t = round(Number(newT));
-            });
-        }, []);
+      setTimeline(
+        animFactory({
+          startTime: scaledStartTime,
+          duration: animationTime,
+          repeatCB,
+          updateCB
+        })
+      );
+    }, [paused, max, animationTime, min]);
 
-        const repeatCB = useCallback((newT) => {
-            setAnimationData((draft) => {
-                draft.t = min;
-            });
-        }, []);
+    return (
+      <div className='flex content-around items-center p-4 text-white'>
+        <Slider value={t} CB={sliderCB} label={'t0'} max={max} min={min} />
+        <span onClick={pauseCB} className='m-4 p-4 border-2 flex-1'>
+          {paused ? '\u{25B6}' : '\u{23F8}'}
+        </span>
+      </div>
+    );
+  };
 
-        const updateCB = useCallback((newTime) => {
-            setAnimationData((draft) => {
-                draft.t = round(newTime * (max - min) + min);
-            });
-        }, []);
-
-        const pauseCB = useCallback(() => {
-            setAnimationData((draft) => {
-                draft.paused = !draft.paused;
-            });
-        }, []);
-
-        //----------------------------------------
-
-        const { min, max } = useAtom(minMaxAtom)[0];
-
-        const [timeline, setTimeline] = useState(null);
-
-        useEffect(() => {
-            if (paused) {
-                if (timeline) {
-                    timeline.pause();
-                }
-                setTimeline(null);
-                return;
-            }
-
-            setTimeline(
-                animFactory({
-                    startTime: (t - min) / (max - min),
-                    duration: animationTime,
-                    repeatCB,
-                    updateCB
-                })
-            );
-        }, [paused, max, animationTime, min]);
-
-        return (
-            <div className='flex content-around items-center p-4 text-white'>
-                <Slider value={t} CB={sliderCB} label={'t0'} max={max} min={min} />
-                <span onClick={pauseCB} className='m-4 p-4 border-2 flex-1'>
-                    {paused ? '\u{25B6}' : '\u{23F8}'}
-                </span>
-            </div>
-        );
-    };
-
-    return { atom: animationAtom, readWriteAtom, component };
+  return { atom: animationAtom, readWriteAtom, component };
 }
--- packages/jotai-data-setup/factories/AnimationFactory.ts
+++ packages/jotai-data-setup/factories/AnimationFactory.ts
@@ -1,31 +1,33 @@
 import gsap from 'gsap';
 
 const animFactory = ({
-    startTime,
-    duration,
-    updateCB,
-    repeatCB = () => null,
-    repeatDelay = 0.75
+  startTime,
+  duration,
+  updateCB,
+  repeatCB = () => null,
+  repeatDelay = 0.75,
+  yoyo = true
 }) => {
-    let animatedObject = { time: 0 };
+  let animatedObject = { time: 0 };
 
-    const tl = gsap.timeline();
+  const tl = gsap.timeline();
 
-    tl.to(animatedObject, {
-        time: 1,
-        ease: 'none',
-        duration,
-        paused: false,
-        repeat: -1,
-        onUpdate: () => updateCB(animatedObject.time)
-    });
+  tl.to(animatedObject, {
+    time: 1,
+    ease: 'none',
+    duration,
+    paused: false,
+    repeat: -1,
+    onUpdate: () => updateCB(animatedObject.time),
+    yoyo
+  });
 
-    tl.pause();
+  tl.pause();
 
-    // the argument should be in seconds
-    tl.play(startTime * duration);
+  // the argument should be in seconds
+  tl.play(startTime * duration);
 
-    return tl;
+  return tl;
 };
 
 export default animFactory;
