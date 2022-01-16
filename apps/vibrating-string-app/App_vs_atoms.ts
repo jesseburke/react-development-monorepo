c1bf0e9f28f5917ceb2ffe99e852c7d07abfc79a
jesseburke <jtb445@gmail.com>
2022-01-16 16:46:03 +1100

    formatting

--- apps/vibrating-string-app/App_vs_atoms.ts
+++ apps/vibrating-string-app/App_vs_atoms.ts
@@ -15,9 +15,9 @@ import { ObjectPoint2, Bounds, CurveData2, LabelStyle, AxesDataT } from '../../m
 // initial constants
 
 const colors = {
-    arrows: '#B01A46', //'#C2374F'
-    solutionCurve: '#4e6d87', //'#C2374F'
-    tick: '#cf6c28' //#e19662'
+  arrows: '#B01A46', //'#C2374F'
+  solutionCurve: '#4e6d87', //'#C2374F'
+  tick: '#cf6c28' //#e19662'
 };
 
 const initBounds: Bounds = { xMin: -1, xMax: 30, yMin: -10, yMax: 10, zMin: -5, zMax: 5 };
@@ -25,30 +25,30 @@ const initBounds: Bounds = { xMin: -1, xMax: 30, yMin: -10, yMax: 10, zMin: -5, 
 const initFuncStr: string = '4*e^(-(x-2*t^2)^2)+sin(x+t)-cos(x-t)'; //'4*e^(-(x-2*t)^2)+sin(x+t)-cos(x-t)';
 
 const initAxesData: AxesDataT = {
-    radius: 0.02,
-    show: true,
-    tickLabelDistance: 0
+  radius: 0.02,
+  show: true,
+  tickLabelDistance: 0
 };
 
 const initXLength = initBounds.xMax - initBounds.xMin;
 const initYLength = initBounds.yMax - initBounds.yMin;
 
 const initCameraData = {
-    target: [5.06, 1.65, 0],
-    //[initXLength * (10.15 / 20), initYLength * (4.39 / 20), 0],
-    position: [-19.73, -22.32, 24.65] //[(-16.7 / 20) * initXLength, (-26.1 / 20) * initYLength, 6.65]
+  target: [5.06, 1.65, 0],
+  //[initXLength * (10.15 / 20), initYLength * (4.39 / 20), 0],
+  position: [-19.73, -22.32, 24.65] //[(-16.7 / 20) * initXLength, (-26.1 / 20) * initYLength, 6.65]
 };
 
 export const labelStyle: LabelStyle = {
-    color: 'black',
-    padding: '.1em',
-    margin: '.5em',
-    fontSize: '1.5em'
+  color: 'black',
+  padding: '.1em',
+  margin: '.5em',
+  fontSize: '1.5em'
 };
 
 const tickLabelStyle = Object.assign(Object.assign({}, labelStyle), {
-    fontSize: '1.5em',
-    color: colors.tick
+  fontSize: '1.5em',
+  color: colors.tick
 });
 
 //------------------------------------------------------------------------
@@ -57,89 +57,95 @@ const tickLabelStyle = Object.assign(Object.assign({}, labelStyle), {
 
 export const labelData = LabelDataComp({ yLabel: 't', twoD: true });
 export const axesData = AxesDataComp({
-    ...initAxesData,
-    tickLabelStyle
+  ...initAxesData,
+  tickLabelStyle
 });
 
 const functionLabelAtom = atom(
-    (get) => 's(' + get(labelData.atom).x + ', ' + get(labelData.atom).y + ') = '
+  (get) => 's(' + get(labelData.atom).x + ', ' + get(labelData.atom).y + ') = '
 );
 
 export const funcData = FunctionDataComp({
-    initVal: initFuncStr,
-    functionLabelAtom,
-    labelAtom: labelData.atom,
-    inputSize: 40
+  initVal: initFuncStr,
+  functionLabelAtom,
+  labelAtom: labelData.atom,
+  inputSize: 40
 });
 
 export const boundsData = BoundsDataComp({
-    initBounds,
-    labelAtom: labelData.atom
+  initBounds,
+  labelAtom: labelData.atom
 });
 
 const xCanvOverhang = 0;
 
 export const canvasBoundsAtom = atom((get) => {
-    const { xMin, xMax, zMin, zMax } = get(boundsData.atom);
+  const { xMin, xMax, zMin, zMax } = get(boundsData.atom);
 
-    return {
-        xMin: xMin - xCanvOverhang,
-        xMax,
-        yMin: zMin,
-        yMax: zMax
-    };
+  return {
+    xMin: xMin - xCanvOverhang,
+    xMax,
+    yMin: zMin,
+    yMax: zMax
+  };
 });
 
 const gridOverhang = 2;
 
 export const gridBoundsAtom = atom((get) => {
-    const { xMin, xMax, yMin, yMax } = get(boundsData.atom);
+  const { xMin, xMax, yMin, yMax } = get(boundsData.atom);
 
-    return {
-        xMin: xMin - gridOverhang,
-        xMax: xMax + gridOverhang,
-        yMin: yMin - gridOverhang,
-        yMax: yMax + gridOverhang
-    };
+  return {
+    xMin: xMin - gridOverhang,
+    xMax: xMax + gridOverhang,
+    yMin: yMin - gridOverhang,
+    yMax: yMax + gridOverhang
+  };
 });
 
-export const animationData = AnimationData({
-    minMaxAtom: atom((get) => {
-        return { min: get(boundsData.atom).yMin, max: get(boundsData.atom).yMax };
-    })
+const minMaxAtom = atom((get) => {
+  const { yMin, yMax } = get(boundsData.atom);
+
+  return { min: yMin, max: yMax };
 });
+
+export const animationData = AnimationData({ minMaxAtom });
 
 export const animationValueAtom = atom((get) => get(animationData.atom).t);
 
 const planeOverhang = 0;
 
 export const planeHeightAndWidthAtom = atom((get) => {
-    const { xMin, xMax, zMin, zMax } = get(boundsData.atom);
+  const { xMin, xMax, zMin, zMax } = get(boundsData.atom);
 
-    return { width: xMax - xMin + planeOverhang, height: zMax - zMin };
+  return { width: xMax - xMin + planeOverhang, height: zMax - zMin };
 });
 
 export const planeCenterAtom = atom((get) => {
-    const { xMin, xMax } = get(boundsData.atom);
+  const { xMin, xMax } = get(boundsData.atom);
 
-    return [(xMax - xMin) / 2 + xMin, get(animationValueAtom)];
+  // const t = get(animationValueAtom);
+
+  // console.log('planeCenterAtom get call; xMin = ', xMin, 'xMax = ', xMax, 't = ', t);
+
+  return [(xMax - xMin) / 2 + xMin, get(animationValueAtom)];
 });
 
 export const twoDFuncAtom = atom((get) => {
-    const t = get(animationValueAtom);
+  const t = get(animationValueAtom);
 
-    return { func: (x) => get(funcData.funcAtom).func(x, t) };
+  return { func: (x) => get(funcData.funcAtom).func(x, t) };
 });
 
 export const cameraData = PerspCameraData(initCameraData);
 
 export const atomStoreAtom = atom({
-    ls: labelData.readWriteAtom,
-    ax: axesData.readWriteAtom,
-    fn: funcData.readWriteAtom,
-    bd: boundsData.readWriteAtom,
-    cd: cameraData.readWriteAtom,
-    ad: animationData.readWriteAtom
+  ls: labelData.readWriteAtom,
+  ax: axesData.readWriteAtom,
+  fn: funcData.readWriteAtom,
+  bd: boundsData.readWriteAtom,
+  cd: cameraData.readWriteAtom,
+  ad: animationData.readWriteAtom
 });
 
 export const DataComp = MainDataComp(atomStoreAtom);
